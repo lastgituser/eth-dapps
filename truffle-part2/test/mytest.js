8bd997084a9a76cb625c41f3a4bce9095ee6bc0a
A B <securionaire@protonmail.com>
2018-12-10 22:37:46 -0500

    Added a couple tests

--- truffle-part2/test/mytest.js
+++ truffle-part2/test/mytest.js
@@ -8,4 +8,21 @@ contract("Hello", (accounts) => {
             assert.equal(balances.valueOf(), 1000, "1000 wasn't in the account");
         });
     });
+
+    it("should return the name \"hello\"", () => {
+        return Hello.deployed().then((instance) => {
+            return instance.getName.call()
+        }).then((name) => {
+            assert.equal(name, "hello", "The name isn't \"hello\"");
+        });
+    });
+
+    it("should return the name \"hi\"", () => {
+        return Hello.deployed().then(async (instance) => {
+            await instance.setName("hi");
+            return instance.getName.call()
+        }).then((name) => {
+            assert.equal(name, "hi", "The name isn't \"hi\"");
+        });
+    });
 });
